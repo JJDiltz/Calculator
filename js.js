9903aaec6b28caa653600485efad7ecf743a855e
jjdiltz <Jdiltz21@gmail.com>
2023-03-10 11:29:03 -0800

    basic layout for html and css done

---
+++ js.js
@@ -0,0 +1,8 @@
+let displayResults = document.querySelector('result')
+
+let clearButton = addEventListener('click', () => {
+    displayResults.value.innerHTML='new'
+    console.log('hello')
+})
+
+
